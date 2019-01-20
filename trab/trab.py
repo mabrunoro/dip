2daccb64e197b50a02b6b5dc3f4bcdc0396abbd9
Marco Brunoro <ma.brunoro@gmail.com>
2019-01-20 19:37:18 -0200

    Done, for now

--- trab/trab.py
+++ trab/trab.py
@@ -149,44 +149,45 @@ def checkcolor(img):
 	else:
 		return None
 
-def prin(img,ori):
+def limpar(img,size=13):
+	for i in range(size,img.shape[0]-size):
+		for j in range(size,img.shape[1]-size):
+			if(img[i,j] and not (np.any(img[i-size:i+size,j-size]) or np.any(img[i-size:i+size,j+size]) or np.any(img[i-size,j-size:j+size]) or np.any(img[i+size,j-size:j+size]))):
+				img[i,j] = False
+	return img
+
+def prin(img,ori,pontos=30):
 	im = np.array(img.convert('L'))
 	k = otsu(im)
 
-	crossimg = lookcross(limiarizar(im,k))
-	# crossimg = lookcross(im)
-	# sumx = 0
-	# sumy = 0
-	# for i,j in crossimg:
-	# 	sumx += i
-	# 	sumy += j
-	# sumx = sumx // 4
-	# sumy = sumy // 4
+	crossimg = lookcross(limiarizar(im,k))	# busca as cruzes na imagem (sem rotacionar)
 
-	angimg = np.arctan((crossimg[0][0]-crossimg[1][0])/(crossimg[0][1]-crossimg[1][1]))*180/math.pi
-	imgrot = img.rotate(angimg)	# img rotacionada colorida
-	res1 = np.array(imgrot)
+	angimg = np.arctan((crossimg[0][0]-crossimg[1][0])/(crossimg[0][1]-crossimg[1][1]))*180/math.pi	# descobre o angulo em relação à original
+	imgrot = img.rotate(angimg)	# rotaciona a imagem colorida
+	res1 = np.array(imgrot)	# array da imagem colorida
 
 	res2 = np.array(imgrot.convert('L'))	# img rotacionada e em escala de cinza - array
-	crossimg = lookcross(limiarizar(res2,k))
-	# crossimg = lookcross(res2)
-	# imgcross = Image.fromarray(res1[crossimg[3][0]:crossimg[0][0],crossimg[3][1]:crossimg[0][1],:])	# img rotacionada colorida - somente plano dos eixos
+	crossimg = lookcross(limiarizar(res2,k))	# obtém as cruzes na imagem rotacionada
+	imgcross = Image.fromarray(res1[crossimg[3][0]:crossimg[0][0],crossimg[3][1]:crossimg[0][1],:])	# separa o plano dos eixos da imagem colorida rotacionada
 
-	orig = ori.convert('L')
-	res3 = limiarizar(np.array(orig),200)	# original binarizada
-	crossori = lookcross(res3)
-	# res4 = dilatar(np.uint8(255)-res3[crossori[3][0]:crossori[0][0],crossori[3][1]:crossori[0][1]],3)
-	# res5 = Image.fromarray(res4)
-	#
-	# res6 = imgcross.resize(res5.size)
-	#
-	# res7 = np.array(res6,dtype=np.int16) + np.array(res5.convert('RGB'),dtype=np.int16)
-	# res8 = np.array(res7.clip(min=0,max=255),dtype=np.uint8)
+	orig = ori.convert('L')	# converte imagem original pra escala de cinza (começa como RGB)
+	res3 = limiarizar(np.array(orig),200)	# imagem original binarizada
+	crossori = lookcross(res3)	# procura as cruzes na imagem original
+	# res4 = np.uint8(255)-res3[crossori[3][0]:crossori[0][0],crossori[3][1]:crossori[0][1]]
+	res4 = dilatar(np.uint8(255)-res3[crossori[3][0]:crossori[0][0],crossori[3][1]:crossori[0][1]],3)
+	res5 = Image.fromarray(res4)
+
+	res6 = imgcross.resize(res5.size)	# iguala o tamanho da imagem ao da imagem original
+	# res5.show()
+	# res6.show()
+
+	res7 = np.array(res6,dtype=np.int16) + np.array(res5.convert('RGB'),dtype=np.int16)	# faz a diferença entre o plano da imagem e o da imagem original (remover eixos)
+	res8 = np.array(res7.clip(min=0,max=255),dtype=np.uint8)	# padroniza valores, removendo os negativos
 	# Image.fromarray(res8).show()
-	#
-	# res9 = afinar(res8,size=3)
+	res9 = afinar(res8,size=3)	# limpa a image resultante
 	# Image.fromarray(res9).show()
 
+	# obtém os dados das curvas: cores, máximos e escalas
 	curvas = [[None,-1,None,-1,None],[None,-1,None,-1,None],[None,-1,None,-1,None]]
 	if(ori.size != img.size):
 		rx = (crossimg[3][0]-crossimg[1][0])/(crossori[3][0]-crossori[1][0])
@@ -248,15 +249,65 @@ def prin(img,ori):
 
 	for i in range(3):
 		print('Curva',i+1)
-		print('\tRGB:'curvas[i][0][0],curvas[i][0][1],curvas[i][0][2])
+		print('\tRGB:',curvas[i][0][0],curvas[i][0][1],curvas[i][0][2])
 		print('\tXmax:',MAXIMOS[curvas[i][1]])
 		print('\tEscala X:','log' if curvas[i][2] else 'linear')
 		print('\tYmax:',MAXIMOS[curvas[i][3]])
 		print('\tEscala Y:','log' if curvas[i][4] else 'linear')
-	Image.fromarray(res1).show()
+	# Image.fromarray(res1).show()
+	# res10 = (limpar(np.any(res9 < np.array([250,250,250]),axis=2))*np.uint8(255)).astype(np.uint8)
+	# Image.fromarray(res10).show()
+	res10 = np.argwhere(limpar(np.any(res9 < np.array([250,250,250]),axis=2)))
+	# res11 = np.zeros(res9.shape,dtype=np.uint8)
+	# for i,j in res10:
+	# 	res11[i,j,:] = res9[i,j,:]
+	# Image.fromarray(res9).show()
+	# Image.fromarray(res11).show()
+	if(curvas[0][0] is not None):
+		if(curvas[1][0] is not None):
+			if(curvas[2][0] is not None):
+				grupos = [[],[],[]]
+			else:
+				grupos = [[],[]]
+		else:
+			grupos = [[]]
+	else:
+		print('Não há curvas identificadas')
+		sys.exit(0)
+	centroX = res6.size[0]
+	centroY = res6.size[1]
+	# for i,j in crossori:
+	# 	centroX += i
+	# 	centroY += j
+	# centroX = centroX // 4
+	# centroY = centroY // 4
+	# DY = 1042
+	# DX = 2084
+	aux6 = np.array(res6)
+	for i,j in res10:
+		dists = np.abs([np.linalg.norm(curvas[k][0].astype(np.int16) - aux6[i,j,:].astype(np.int16)) for k in range(len(grupos))])
+		corp = np.argpartition(dists,(0,1))
+		if(np.abs(dists[corp[0]]-dists[corp[1]]) < 50):
+			varponto = np.var(aux6[i,j,:])
+			var0 = np.var(curvas[corp[0]][0])
+			var1 = np.var(curvas[corp[1]][0])
+			cor = corp[0] if (np.abs(varponto - var0) < np.abs(varponto - var1)) else corp[1]
+		else:
+			cor = corp[0]
+		X = ('10^'+str((j-centroY)*MAXIMOS[curvas[cor][1]]/2084)) if curvas[cor][2] else str((j-centroY)*MAXIMOS[curvas[cor][1]]/2084)
+		Y = ('10^'+str((centroX-i)*MAXIMOS[curvas[cor][3]]/1042)) if curvas[cor][4] else str((centroX-i)*MAXIMOS[curvas[cor][3]]/1042)
+		grupos[cor].append([X,Y])	# X e Y são trocados no numpy
+
+	for i in range(len(grupos)):
+		with open('output/curva'+str(i)+'.txt','w') as f:
+			f.write('Cor RGB: '+str(curvas[i][0]))
+			# print(len(grupos[i]))
+			passo = (len(grupos[i])//pontos) if (pontos < len(grupos[i])) else 1
+			for j in range(0,len(grupos[i]),passo):
+				f.write(str(grupos[i][j]))
 
 
-def main(path='/Users/mthome/Dropbox/UFES/Processamento Digital de Imagens/trab/'):
+def main(pontos=30,path='/Users/mthome/Dropbox/UFES/Processamento Digital de Imagens/trab/'):
 	try:
 		os.mkdir(path+'output/')
 	except FileExistsError as e:
@@ -267,11 +318,11 @@ def main(path='/Users/mthome/Dropbox/UFES/Processamento Digital de Imagens/trab/
 	# im = pngtorgb(Image.open(path+'G0.png'))
 	# im = Image.open(path+'G1.png')
 	im = Image.open(path+'G2.png')
-	prin(im,orig)
+	prin(im,orig,pontos)
 
-	# im.rotate(ang*180/math.pi).show()
-
-	# prin(im)
 
 if(__name__ == '__main__'):
-	main()
+	if(len(sys.argv) > 1):
+		main(int(sys.argv[1]))
+	else:
+		main()
